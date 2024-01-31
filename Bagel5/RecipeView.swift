39f71aafa474672581877a6c518c90dab7ef3590
fahadmunawar <fahadshaikh006@gmail.com>
2024-01-31 12:12:58 -0500

    updates

--- Bagel5/RecipeView.swift
+++ Bagel5/RecipeView.swift
@@ -8,26 +8,103 @@
 import Foundation
 import SwiftUI
 
+let mess = "chicken eggs cheese"
+
 struct RecipeView: View {
+    
+    
+    @State private var ingredients: String = ""
+    @State private var recipe: String = ""
+    @State private var instructions: String = ""
+    @State private var timeToCook: String = ""
+    @State private var description: String = ""
+    
     
     var body: some View {
         NavigationStack {
-            ScrollView(.vertical) {
-                VStack (alignment: .leading) {
-                    Text("Recipe")
-                        .font(.largeTitle)
+            ZStack{
+                Color(CustomColor.background)
+                    .ignoresSafeArea()
+                ScrollView(.vertical) {
+                    VStack (alignment: .leading) {
+                        VStack (alignment: .leading){
+                            
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                Image(systemName: "bookmark")
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                                    .padding()
+                                    .foregroundColor(.white)
+                            })
+                            Text(recipe)
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            Text("Time\n\(timeToCook)")
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                            
+                        }
                         .padding()
-                    
-                    
+                        .background {
+                            Color(CustomColor.lightGreen)
+                                .ignoresSafeArea()
+                        }
+                        
+                        VStack (alignment: .leading){
+                            Spacer()
+                            Divider()
+                            
+                            Text("Description\n\(description)")
+                            
+                            Spacer()
+                            Divider()
+                            
+                            Text("Ingredients\n")
+                            let components = ingredients.components(separatedBy: " ")
+                            HStack {
+                                ForEach(components, id: \.self) { ingre in
+                                    Text(ingre)
+                                        .multilineTextAlignment(.leading)
+                                        .padding()
+                                        .background {
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .fill(.white)
+                                        }
+                                    
+                                }
+                            }
+                            
+                            Spacer()
+                            Divider()
+                            Text("Instructions\n\(instructions)")
+                                .font(.subheadline)
+                            
+                            
+                        }
+                    }
+                    .padding()
                 }
             }
         }
         .task {
             do {
-                try await OpenAIService.shared.sendPromptToChatGPT(message: "chicken tortilla eggs and cheese")
+                let (instructions, ingredients, recipe, timeToCook, description) = try await OpenAIService.shared.sendPromptToChatGPT(message: mess)
+                
+                self.recipe = recipe
+                self.timeToCook = timeToCook
+                self.ingredients = ingredients
+                self.instructions = instructions
+                self.description = description
+                
             } catch {
                 print(error.localizedDescription)
             }
         }
     }
+}
+
+#Preview {
+    RecipeView()
 }
